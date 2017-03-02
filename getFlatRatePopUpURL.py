8dcc219c62c983371eda62eea7a0024a5531e147
JohnJeong123 <frenchofkiss@gmail.com>
2017-03-02 16:18:00 +0900

    added UserID overload func

--- getFlatRatePopUpURL.py
+++ getFlatRatePopUpURL.py
@@ -24,10 +24,7 @@ try:
     # 팝빌회원 사업자번호
     CorpNum = testValue.testCorpNum
 
-    # 팝빌회원 아이디
-    UserID = testValue.testUserID
-
-    url = htCashbillService.getFlatRatePopUpURL(CorpNum, UserID)
+    url = htCashbillService.getFlatRatePopUpURL(CorpNum)
 
     print("URL : %s" % url)
 
