eb3330c88c57d36fceda83a519894acbd612ea15
정우석 <wsj@linkhubcorp.com>
2025-01-20 19:34:55 +0900

    V24-11-07 수정

--- requestJob.py
+++ requestJob.py
@@ -33,10 +33,10 @@ try:
     Type = "SELL"
 
     # 시작일자, 날짜형식(yyyyMMdd)
-    SDate = "20220701"
+    SDate = "20241201"
 
     # 종료일자, 날짜형식(yyyyMMdd)
-    EDate = "20220731"
+    EDate = "20241231"
 
     jobID = htCashbillService.requestJob(CorpNum, Type, SDate, EDate)
 
